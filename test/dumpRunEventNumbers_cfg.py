0d7cc9630c2752a3ab8c63b093446ed456b01db9
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2009-10-27 16:15:31 +0000

    removed TH1::Sumw2 calls (now directly done at time of histogram booking)

--- test/dumpRunEventNumbers_cfg.py
+++ test/dumpRunEventNumbers_cfg.py
@@ -11,22 +11,31 @@ process = cms.Process('dumpRunEventNumbers')
 process.DQMStore = cms.Service("DQMStore")
 
 process.maxEvents = cms.untracked.PSet(            
-  input = cms.untracked.int32(0)         
+    input = cms.untracked.int32(0)         
 )
 
 process.source = cms.Source("EmptySource")
 
 process.loadFilterStatistics = cms.EDAnalyzer("DQMFileLoader",
-  tmp = cms.PSet(
-    inputFileNames = cms.vstring('../../Configuration/test/plotsZtoMuTau_Ztautau_part01.root',
-                                 '../../Configuration/test/plotsZtoMuTau_Ztautau_part02.root'),
-    dqmDirectory_store = cms.string('tmp')
-  )
+    dump = cms.PSet(
+        #inputFileNames = cms.vstring('../../Configuration/test/plotsZtoElecTau_all.root'),
+        inputFileNames = cms.vstring(
+            #'rfio:/castor/cern.ch/user/l/lusito/Plots/ZmumuForChristian/plotsZtoMuTau_Zmumu_part01.root',
+            #'rfio:/castor/cern.ch/user/l/lusito/Plots/ZmumuForChristian/plotsZtoMuTau_Zmumu_part02.root',
+            #'rfio:/castor/cern.ch/user/l/lusito/Plots/ZmumuForChristian/plotsZtoMuTau_Zmumu_part03.root'
+            #'../../Configuration/test/plotsZtoElecTau_failedElecEcalIsolation.root'
+            #'../../Configuration/test/plotsZtoMuTau_all.root'
+            '../../Configuration/test/plotsWtoTauNu_all.root'
+        ),
+        dqmDirectory_store = cms.string('')
+    )
 )
 
 process.dumpRunEventNumbers = cms.EDAnalyzer("DQMDumpRunEventNumbers",
-  dqmDirectories = cms.vstring('tmp/zMuTauAnalyzer/FilterStatistics/evtSelDiTauCandidateForMuTauMt1MET/events_passed_cumulative/'),
-  runEventNumberFileName = cms.string("selEvents_zMuTau.txt")
+    #dqmDirectories = cms.vstring('harvested/Zmumu/zMuTauAnalyzer/FilterStatistics/evtSelDiTauCandidateForMuTauPzetaDiff/events_passed_cumulative/'),
+    #runEventNumberFileName = cms.string("selEvents_ZtoMuTau_Zmumu_passed.txt")
+    dqmDirectories = cms.vstring('harvested/qcd_W/wTauNuAnalyzer/FilterStatistics/evtSelTauCharge/events_passed_cumulative/'),
+    runEventNumberFileName = cms.string("selEvents_WtoTauNu_QCD_passed.txt")                                         
 )
  
 process.p = cms.Path( process.loadFilterStatistics
