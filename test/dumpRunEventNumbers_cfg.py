7908a4a126f187a18c5c8c73cb11eb80c00600f7
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2010-01-25 17:16:48 +0000

    minor clean-up

--- test/dumpRunEventNumbers_cfg.py
+++ test/dumpRunEventNumbers_cfg.py
@@ -18,25 +18,22 @@ process.source = cms.Source("EmptySource")
 
 process.loadFilterStatistics = cms.EDAnalyzer("DQMFileLoader",
     dump = cms.PSet(
-        #inputFileNames = cms.vstring('../../Configuration/test/plotsZtoElecTau_all.root'),
         inputFileNames = cms.vstring(
-            #'rfio:/castor/cern.ch/user/l/lusito/Plots/ZmumuForChristian/plotsZtoMuTau_Zmumu_part01.root',
-            #'rfio:/castor/cern.ch/user/l/lusito/Plots/ZmumuForChristian/plotsZtoMuTau_Zmumu_part02.root',
-            #'rfio:/castor/cern.ch/user/l/lusito/Plots/ZmumuForChristian/plotsZtoMuTau_Zmumu_part03.root'
-            #'../../Configuration/test/plotsZtoElecTau_failedElecEcalIsolation.root'
-            #'../../Configuration/test/plotsZtoMuTau_all.root'
-            '../../Configuration/test/plotsWtoTauNu_all.root'
+            '../../Configuration/test/plotsZtoMuTau_InclusivePPmuXSum.root'
         ),
-        dqmDirectory_store = cms.string('')
+        dqmDirectory_store = cms.string('dump')
     )
 )
 
+process.dumpDQMStore = cms.EDAnalyzer("DQMStoreDump")
+
 process.dumpRunEventNumbers = cms.EDAnalyzer("DQMDumpRunEventNumbers",
-    #dqmDirectories = cms.vstring('harvested/Zmumu/zMuTauAnalyzer/FilterStatistics/evtSelDiTauCandidateForMuTauPzetaDiff/events_passed_cumulative/'),
-    #runEventNumberFileName = cms.string("selEvents_ZtoMuTau_Zmumu_passed.txt")
-    dqmDirectories = cms.vstring('harvested/qcd_W/wTauNuAnalyzer/FilterStatistics/evtSelTauCharge/events_passed_cumulative/'),
-    runEventNumberFileName = cms.string("selEvents_WtoTauNu_QCD_passed.txt")                                         
+    dqmDirectories = cms.vstring('dump/InclusivePPmuX/zMuTauAnalyzer_factorizedWithoutMuonIsolation/FilterStatistics/evtSelDiMuPairZmumuHypothesisVeto/events_passed_cumulative/'),
+    runEventNumberFileName = cms.string("selEvents_ZtoTauNu_InclusivePPmuX_passed.txt")                                         
 )
  
-process.p = cms.Path( process.loadFilterStatistics
-                     +process.dumpRunEventNumbers )
+process.p = cms.Path(
+    process.loadFilterStatistics
+   + process.dumpDQMStore 
+   + process.dumpRunEventNumbers
+)
