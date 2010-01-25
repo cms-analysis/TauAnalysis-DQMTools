e59e74c89ed3d637520276e1f49565f7c3dfb2f8
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2010-01-25 17:17:41 +0000

    - moved drawJobConfigurator.py into tools subdirectory - moved composeSubDirectoryName from drawJobConfigurator.py into separate function

--- python/tools/drawJobConfigurator.py
+++ python/tools/drawJobConfigurator.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 import copy
-import sys
+
+from TauAnalysis.DQMTools.tools.composeSubDirectoryName import composeSubDirectoryName
 
 #--------------------------------------------------------------------------------
 # utility function for generation of drawJob configurations
@@ -20,35 +21,6 @@ class drawJobConfigurator(cms._ParameterTypeBase):
             self.dqmDirectory += "/"
 
         self.drawJobs = cms.PSet()
-
-    @staticmethod
-    def _getCutName(cut):
-        if ( isinstance(cut, cms.PSet) ):
-            return getattr(cut, "pluginName").value()
-        elif ( isinstance(cut, cms.string) ):
-            return cut.value()
-        elif ( isinstance(cut, str) ):            
-            return cut
-        else:
-            raise ValueError("Invalid type for 'cut' Parameter !!")
-
-    @staticmethod
-    def _composeSubDirectoryName(afterCut = None, beforeCut = None):
-        # auxiliary function to compose name of dqmSubDirectory
-        # in which histograms filled after applying afterCut,
-        # but before applying beforeCut, are stored
-
-        dqmSubDirectory = ""
-        if ( afterCut is not None ):
-            afterCut_name = drawJobConfigurator._getCutName(afterCut)
-            dqmSubDirectory += "after" + afterCut_name[0:1].capitalize() + afterCut_name[1:]
-        if ( beforeCut is not None ):
-            beforeCut_name = drawJobConfigurator._getCutName(beforeCut)
-            if dqmSubDirectory != "":
-                dqmSubDirectory += "_"
-            dqmSubDirectory += "before" + beforeCut_name[0:1].capitalize() + beforeCut_name[1:]
-
-        return dqmSubDirectory;
 
     def add(self, afterCut = None, beforeCut = None, plot = None, plots = None):
         # configure drawJob
@@ -72,7 +44,7 @@ class drawJobConfigurator(cms._ParameterTypeBase):
         drawJob = copy.deepcopy(self.template)
 
         dqmDirectory = self.dqmDirectory
-        dqmDirectory += drawJobConfigurator._composeSubDirectoryName(afterCut = afterCut, beforeCut = beforeCut)
+        dqmDirectory += composeSubDirectoryName(afterCut = afterCut, beforeCut = beforeCut)
         if not dqmDirectory.endswith("/"):
             dqmDirectory += "/"
 
